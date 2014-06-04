a3f06ca0ffa4dc0523cb332c8b2edd3ca48346d2
Mark Hayes <mark@zurb.com>
2014-06-04 11:42:48 -0700

    added first round of examples

---
+++ js/example_4.js
@@ -0,0 +1,24 @@
+function toggleClass(el, className) {
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+
+    el.className = classes.join(' ');
+  }
+}
+
+var menu = document.querySelector("#offcanvas-menu"),
+    container = document.querySelector(".offcanvas-container");
+
+menu.addEventListener("click", function(e){
+  e.preventDefault();
+  toggleClass(menu, "active");
+  toggleClass(container, "active");
+});
